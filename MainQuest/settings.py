3f293a2ddee6f0b4615dc121a61c0ee71597ca63
dennyciccia <ciccia.denny@gmail.com>
2024-06-13 12:31:32 +0200

    creata app utenti

--- MainQuest/settings.py
+++ MainQuest/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    "prodotti.apps.ProdottiConfig"
+    "prodotti.apps.ProdottiConfig",
+    "utenti.apps.UtentiConfig"
 ]
 
 MIDDLEWARE = [
