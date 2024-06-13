c6ded247b18149edad66479b12e4c78b432b74ed
dennyciccia <ciccia.denny@gmail.com>
2024-06-13 16:29:16 +0200

    aggiunti models utenti

--- utenti/models.py
+++ utenti/models.py
@@ -1,3 +1,18 @@
+from abc import ABC
 from django.db import models
+from prodotti.models import Prodotto
 
 # Create your models here.
+class Utente(models.Model):
+    nome = models.CharField(max_length=100)
+    foto_profilo = models.ImageField()
+    biografia = models.TextField()
+    prodotti = models.ManyToManyField(Prodotto, related_name="acquirenti")
+
+class Editore(models.Model):
+    nome = models.CharField(max_length=100)
+    foto_profilo = models.ImageField()
+
+class Sviluppatore(models.Model):
+    nome = models.CharField(max_length=100)
+    foto_profilo = models.ImageField()
